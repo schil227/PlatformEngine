a64a635767002019605f71d2b6e20f781670e42d
schil227 <schil227@morris.umn.edu>
2014-02-17 10:50:35 -0600

    Behold, a breakthrough. So screw minitest, it doesnt work with this version of eclipse. What does work is to require 'test/unit' and 'class *Test < Test::Unit::TestCase'. Also, gotta run it a script, not a test (of course.) This may be the start of something beautiful.

--- PlatformerEngine/PositionTest.rb
+++ PlatformerEngine/PositionTest.rb
@@ -1,7 +1,8 @@
 require "./Position"
-gem "minitest"
-require 'minitest/autorun'
-class PositionTest < MiniTest::Test
+#gem "minitest"
+require 'test/unit'
+
+class PositionTest < Test::Unit::TestCase
   def testGetCord
       assert_equal([2,4], Position.new(2,4).getCord())
       assert_equal([4,4], Position.new(4,4).getCord())
